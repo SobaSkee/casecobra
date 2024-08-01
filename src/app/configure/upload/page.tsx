0a10eae23645ed255cb877e73f3a6c8dba0c122f
Stanley Ke <kestanley101@gmail.com>
2024-07-31 22:12:25 -0400

    update

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -14,7 +14,7 @@ const Page = () => {
   const [uploadProgress, setUploadProgress] = useState<number>(0);
   const router = useRouter();
 
-  const { startUpload } = useUploadThing("imageUploader", {
+  const { startUpload, isUploading } = useUploadThing("imageUploader", {
     onClientUploadComplete: ([data]) => {
       const configId = data.serverData.configId;
       startTransition(() => {
@@ -42,7 +42,6 @@ const Page = () => {
     setIsDragOver(false);
   };
 
-  const isUploading = false;
   const [isPending, startTransition] = useTransition();
 
   return (
